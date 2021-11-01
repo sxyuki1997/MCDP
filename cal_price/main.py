67074cc56f205efe2deece8b2b8216ba1faa6a11
sxyuki1997 <35369193+sxyuki1997@users.noreply.github.com>
2021-11-01 16:45:42 +0800

    完善信息熵的计算，增加attack文件

--- cal_price/main.py
+++ cal_price/main.py
@@ -29,6 +29,22 @@ def _cal_type_weight(attrList):
 
 # 目标属性集占所有属性的比重，通过信息熵来确定，每个属性下所含的信息熵
 # 目标属性信息熵 / 总的数据集的信息熵
+def calc_ent(x):
+    """
+        calculate shanno ent of x
+    """
+    x = np.array(x)
+    x_value_list = set([x[i] for i in range(x.shape[0])])
+    ent = 0.0
+    for x_value in x_value_list:
+        p = float(x[x == x_value].shape[0]) / x.shape[0]
+        logp = np.log2(p)
+        ent -= p * logp
+
+    return ent
+
+# x = np.array([1,0,1,0,1,0,1])
+# print(calc_ent(x))
 
 def _cal_entropy(data, targetA, A):
     A = A.tolist()
@@ -40,23 +56,26 @@ def _cal_entropy(data, targetA, A):
 
     for a in range(len(A)):
         tmp = data[A[a]].tolist()
-        n = len(tmp)
-        for i in tmp:
-            pr = tmp.count(i) / n
-            h[a] += - pr * np.log(pr)
+        ent = calc_ent(tmp)
+        h[a] = ent
 
     for i in t:
         sum_t_h += h[i]
 
     hw = sum_t_h / sum(h)
 
-    print(hw)
+    print("属性权重因子hw：",hw)
+    print("信息熵矩阵h：", h)
     return hw
 
-
+# 计算攻击成功概率
+def _cal_attack_p(p):
+    # 拟合曲线
+    prob = 0.84 * np.e**(-2.44*p) + 0.13
+    return prob
                  
 def main():
-    data_url = "../data/income.csv"
+    data_url = "../data/adult_2.csv"
     data = pd.read_csv(data_url)
     # 开始时间
     startT = time.time()*1000 #毫秒数
@@ -78,10 +97,12 @@ def main():
     # 信息熵计算
     hw = _cal_entropy(data, ['age', 'income'], A)
     p = p*hw
+    prob = _cal_attack_p(p)
 
     endT = time.time() * 1000
     interval = endT - startT
-    print(interval)
-    print(p)
+    print("运行时间：",interval)
+    print("数据价值：",p)
+    print("攻击成功概率", prob)
 
 main()
---
+++ cal_price/plot.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.rcParams['font.sans-serif'] = ['STZhongsong']
+plt.rcParams['axes.unicode_minus'] = False
+plt.rcParams['figure.dpi'] = 600
+plt.rcParams['figure.figsize'] = (6,5)
+
+x = [i*0.01 for i in range(100)]
+
+y = []
+for i in x:
+    if i == 0:
+        y.append(0)
+    else:
+        y.append(-i*np.log2(i)-(1-i)*np.log2(1-i))
+
+x.append(1)
+y.append(0)
+
+# plt.plot(x,y)
+# plt.xlabel("p(x)")
+# plt.ylabel("H(X)")
+# plt.grid()
+# plt.title("信息熵与事件发生概率")
+# plt.savefig('../img/entropy.png')
+# plt.show()
+
