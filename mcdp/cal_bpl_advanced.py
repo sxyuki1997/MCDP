caa7269cfd8cf721250211e09872b12da5bb2bd9
sxyuki1997 <35369193+sxyuki1997@users.noreply.github.com>
2021-11-17 23:31:13 +0800

    feat: 修正分布方差

--- mcdp/cal_bpl_advanced.py
+++ mcdp/cal_bpl_advanced.py
@@ -5,9 +5,9 @@ import time
 plt.rcParams['font.sans-serif'] = ['STZhongsong']
 plt.rcParams['axes.unicode_minus'] = False
 plt.rcParams['figure.dpi'] = 300
-plt.figure(figsize=(10, 8))
+plt.figure(figsize=(6, 5))
 
-eps = 6.66
+eps = 3.25
 
 def calBPL(Pt, a, e, T):
     BPL = [a]
@@ -70,11 +70,13 @@ def generateLoc(n, T):
     for t in range(T):
         m = list()
         for i in range(n):
-            b = np.random.dirichlet(np.ones(n), size=1)
-            b = np.around(b, 3).tolist()[0]
-            while 0.00 in b:
-                b = np.random.dirichlet(np.ones(n), size=1)
-                b = np.around(b, 3).tolist()[0]
+            b = np.random.normal(loc=5, scale=1.5, size=65)
+            b = np.round(b, 3).tolist()
+            for k in range(len(b)):
+                b[k] = np.round(b[k]/np.sum(b), 3)
+            # while 0.00 in b:
+            #     b = np.random.dirichlet(np.ones(n), size=1)
+            #     b = np.around(b, 3).tolist()[0]
 
             m.append(b)
 
@@ -83,17 +85,46 @@ def generateLoc(n, T):
 
     return res
 
+# 0.002 * 20 + 0.001 * 20 + 0.025*10 + 0.0047 * 15
+#
+# pb = []
+# pb.extend([0.002 for i in range(20)])
+# pb.extend(0.001 for i in range(20))
+# pb.extend([0.025 for i in range(10)])
+# pb.extend([0.0047 for i in range(15)])
+#
+#
+# pb = [pb for i in range(65)]
+
+
 # 65个位置之间的关联
 # P = [[1/65 for i in range(65)] for k in range(65)]
-P = generateLoc(65,1)[0]
-
-
+PB = generateLoc(65,1)[0]
+PF = generateLoc(65,1)[0]
+print(PB)
+#
 T = 12
 startT = time.time()
-bpl = calBPL(P, eps , eps, T)
+bpl = calBPL(PB, eps , eps, T)
+fpl = calBPL(PF, eps, eps, T)
+fpl.reverse()
+tpl = []
+for i in range(len(bpl)):
+    tpl.append(bpl[i] + fpl[i] - eps)
+print("bpl", bpl)
+print("fpl", fpl)
+print("TPL:", tpl)
+print("maxpl:", max(tpl))
 endT = time.time()
 interval = endT - startT
 print("=====运行时间=======：", interval * 1000)
-print("BPL:", bpl)
-for item in P:
-    print(item)
+
+plt.plot(bpl, marker='^', label = '$MCBPL_t$')
+plt.plot(fpl, marker='s', label = '$MCFPL_t$')
+plt.plot(tpl, marker='o', label = '$MCDPL_t$')
+plt.plot([eps for i in range(12)], marker='*', label = '$\epsilon_t$')
+plt.title("不同时刻区间下的隐私泄露情况")
+plt.xlabel("时刻区间")
+plt.ylabel("隐私泄露")
+plt.legend()
+plt.show()
