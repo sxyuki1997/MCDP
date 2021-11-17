0089af3909a918811c4f07dd0710c1bc3c1f85be
sxyuki1997 <35369193+sxyuki1997@users.noreply.github.com>
2021-11-17 19:28:03 +0800

    feat: 完善实验

--- mcdp/noised_loc.py
+++ mcdp/noised_loc.py
@@ -19,13 +19,27 @@ def geo_mech(lamda):
         add_noise = np.log((np.log(lamda) * (1 - lamda) * u1 - np.log(lamda) * (1 - lamda))) / np.log(lamda)
     # print(add_noise)
     if add_noise <= 0:
-        add_noise = 0
+        add_noise = -int(np.random.random()*1)
+
     return add_noise
+
+# laplace加噪
+def _lapalce_mech(e):
+    beta = 1 / e
+    u1 = np.random.random()
+    u2 = np.random.random()
+    if u1 < 0.5:
+        noise = int(beta * np.log(2 * u2))
+    else:
+        noise = int(-beta * np.log(2 - 2 * u2))
+    if noise <= -3:
+        noise = -3
+    return noise
 
 def geo_hist_generate(hist, lamda):
     noised_hist = []
     for i in hist:
-        noised_hist.append(geo_mech(lamda) + i)
+        noised_hist.append(_lapalce_mech(lamda) + i)
     return noised_hist
 
 
@@ -43,14 +57,14 @@ def clean_origin():
         hist = origin_time_locs[i]
         plt.subplot(2, 6, i + 1)
         plt.bar(height=hist, x=[k for k in range(1, 66)], width=1, color='SteelBlue', edgecolor="black")
-        plt.title(titles[i] + "时刻区间")
+        plt.title("$t=$" + str(i))
         plt.tight_layout()
-    np.save("origin_time_locs_mod.npy", origin_time_locs)
-
+    # np.save("origin_time_locs_mod.npy", origin_time_locs)
     plt.show()
+    return origin_time_locs
 
 # clean_origin()
-origin_time_locs=np.load("origin_time_locs_mod.npy")
+# origin_time_locs=np.load("origin_time_locs_mod.npy")
 
 def plot_origin(origin_time_locs):
     plt.rcParams['figure.figsize'] = (15, 8)
@@ -62,7 +76,7 @@ def plot_origin(origin_time_locs):
         plt.subplot(6,11,k+1)
         x = [i for i in range(12)]
         plt.bar(x=x, height=tmp, color='SteelBlue', edgecolor = "black")
-        plt.title("位置簇" + str(k+1))
+        plt.title("$loc$" + str(k+1))
         plt.tight_layout()
 
         # # 计算两个直方图之间的距离
@@ -73,31 +87,36 @@ def plot_origin(origin_time_locs):
     plt.show()
     # return origin_time_locs
 
-plot_origin(origin_time_locs)
+# plot_origin(origin_time_locs)
 
 
 
 def main():
     # clean_origin()
     # plot_origin()
-    origin_time_locs=np.load("origin_time_locs_mod.npy")
+    # origin_time_locs=np.load("origin_time_locs_mod.npy")
     # plot_origin()
+    origin_time_locs = clean_origin()
     WD = []
     ACC = []
     MSE = []
-    lamda = 0.3
+
     plt.rcParams['figure.figsize'] = (12, 4)
+    Lamda = [0.35, 0.34, 0.35, 0.37, 0.37, 0.36, 0.36, 0.38, 0.36, 0.33, 0.35, 0.32]
     noised_time_locs = []
 
     for j in range(len(origin_time_locs)):
+        lamda = Lamda[j]
+        eps = round(-np.log(lamda),2)
+
         hist = origin_time_locs[j]
-        noised_hist = geo_hist_generate(hist, lamda)
+        noised_hist = geo_hist_generate(hist, eps)
         noised_time_locs.append(noised_hist)
+
         plt.subplot(2,6,j+1)
         plt.bar(height=noised_hist, x = [k for k in range(1, 66)], width = 1, color='SteelBlue', edgecolor = "black")
         plt.tight_layout()
-        # plt.legend()
-        plt.title(titles[j] + "时刻区间")
+        plt.title("$t=$" + str(j) + ", $\epsilon=$" + str(eps) )
 
         # 计算两个直方图之间的距离
         res_utility = utility.Utility(hist, noised_hist)
@@ -124,6 +143,55 @@ def main():
     print("精度：", ACC)
     print("MSE：", MSE)
 
+
+    plt.rcParams['figure.figsize'] = (10, 8)
+    plt.subplot(2,2,1)
+
+    degree = [5, 5, 14, 13, 13, 14, 17, 12, 4, 3, 2, 1]
+    lalist = [0.26, 0.19, 0.3, 0.41, 0.43, 0.37, 0.35, 0.44, 0.33, 0.17, 0.28, 0.08]
+    plt.scatter(degree, lalist, marker='o', label="$l_A$")
+    plt.title("(1) 用户关联程度与关联泄露系数")
+    plt.xlabel("用户关联程度（max_degree）")
+    plt.ylabel("关联泄露系数（$l_A$）")
+    plt.legend()
+    plt.tight_layout()
+
+
+
+    plt.subplot(2,2,2)
+    plt.plot(WD, label="wasserstein distance", marker="s")
+    mean_wd = np.mean(WD)
+    plt.plot([mean_wd for k in range(12)], marker='_', linestyle= 'dotted',label="均值")
+    plt.xlabel("时刻区间")
+    plt.ylabel("W距离（wasserstein distance）")
+    plt.title("(2) 加噪前后直方图分布的距离")
+    plt.legend()
+
+    plt.tight_layout()
+
+    plt.subplot(2,2,3)
+    plt.plot(ACC, label="精度", marker="^")
+    mean_acc = np.mean(ACC)
+    plt.plot([mean_acc for k in range(12)], marker='_', linestyle= 'dotted',label="均值")
+    plt.xlabel("时刻区间")
+    plt.ylabel("精度")
+    plt.title("(3) 直方图的平均精度")
+    plt.legend()
+
+    plt.tight_layout()
+
+    plt.subplot(2,2,4)
+    plt.plot(MSE, label="MSE", marker='*')
+    mean_mse = np.mean(MSE)
+    plt.plot([mean_mse for k in range(12)], marker='_', linestyle= 'dotted', label="均值")
+    plt.xlabel("时刻区间")
+    plt.ylabel("MSE")
+    plt.title("(4) 直方图发布的MSE")
+    plt.tight_layout()
+    plt.legend()
+    plt.show()
+
+    plot_origin(origin_time_locs)
     plot_origin(noised_time_locs)
 
-# main()
+main()
